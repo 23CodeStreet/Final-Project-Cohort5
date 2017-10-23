a059223f3a2735b2cdd8057a718da6d7e89b8013
VT <marfat.vt@gmail.com>
2017-10-23 22:57:28 +0100

    Added comments for those that werent in the class

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-$(document).ready(function(){
+// Wait till document is finished loading.
+$(document).ready(function() {
+    // Attach a click event to the buttons.
     $(".js-activator").on("click", function(){
+
+        // Hide all sections in main
         $("main > section").hide();
 
+        // Use the attribute  "data-section" to get the related section.
         var index = $(this).attr("data-section");
 
-        $("." + index).show();
-        
-        console.log(index)
+        // Construct the class name.
+        var section = "." + index;
+
+        // Show related section.
+        $(section).show();
     })
 });
-
