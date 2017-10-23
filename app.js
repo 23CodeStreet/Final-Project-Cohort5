22d2d9666dfb76edef2b8fdb290f2f3f8b48647a
Lamis Serroukh <lamisserroukh@Lamiss-MacBook-Pro.local>
2017-10-23 21:34:53 +0100

    added show and hide functionality

---
+++ app.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $(".js-activator").on("click", function(){
+        $("main > section").hide();
+
+        var index = $(this).attr("data-section");
+
+        $("." + index).show();
+        
+        console.log(index)
+    })
+});
+
