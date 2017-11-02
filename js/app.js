80def39f91d7ebfe2e0788b768cfb9167273a7ee
A Vidmar <anja_vidmar@yahoo.co.uk>
2017-11-02 17:27:37 +0000

    add files

---
+++ js/app.js
@@ -0,0 +1,35 @@
+//  function burgerMenu(x) {
+//    x.classList.toggle("change");
+//    $("nav").slideToggle(1000);
+//  }
+
+// Wait till document is finished loading.
+$(document).ready(function() {
+  
+
+
+  // Animated dropdown menu
+  $(".dropbtn").on("click", function(){
+    $(this).toggleClass("change");
+    $("nav").slideToggle(800);
+  });
+
+  // SHOW & HIDE sections in form.html
+  // Attach a click event to the buttons.
+  $(".js-activator").on("click", function(){
+
+    // Hide all sections in main
+    $("main > section").hide();
+
+    // Use the attribute  "data-section" to get the related section.
+    var index = $(this).attr("data-section");
+
+    // Construct the class name.
+    var section = "." + index;
+
+    // Show related section.
+    $(section).show();
+
+  });
+
+});/* <- document ready function */
